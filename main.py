dd2c48d32c7e8dc588f97dcb340d5091005ca23f
= <->
2018-04-18 07:59:23 +0700

    [ draft combine EditPanel ][ change UserListView's splitter layout ]

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from TWidget import LoginDialog, UserListView
-from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QHBoxLayout, QSplitter
+from TWidget import LoginDialog, UserListView, EditPanel
+from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QHBoxLayout, QSplitter, QLineEdit, QTextEdit
 from PyQt5.QtCore import Qt
 from qtmodern import styles, windows
 import sys
@@ -9,6 +9,8 @@ class ManageUserWindow(QWidget):
     def __init__(self):
         super(ManageUserWindow, self).__init__()
         self.setWindowTitle("Asset Manager Admin")
+        self.setMinimumWidth(800)
+        self.setMinimumHeight(600)
 
         self.adminListView = UserListView()
         self.adminListView.setTitleText("Admin User List")
@@ -18,12 +20,23 @@ class ManageUserWindow(QWidget):
         self.creatorListView.setTitleText("Creator User List")
         self.creatorListView.setIcon("img/artist-icon.png")
 
-        self.splitter = QSplitter(Qt.Horizontal)
+        self.editPanel = EditPanel()
+        self.editPanel.setFixedWidth(270)
+        self.editPanel.addEditRow('Employment ID', QLineEdit)
+        self.editPanel.addEditRow('Display Name', QLineEdit)
+        self.editPanel.addEditRow('Username', QLineEdit)
+        self.editPanel.addEditRow('Password', QLineEdit)
+        self.editPanel.addEditRow('Email', QLineEdit)
+        self.editPanel.addEditRow('Notes', QTextEdit)
+
+        self.splitter = QSplitter(Qt.Vertical)
         self.splitter.addWidget(self.adminListView)
         self.splitter.addWidget(self.creatorListView)
+        # self.splitter.addWidget(self.editPanel)
 
         layout = QHBoxLayout(self)
         layout.addWidget(self.splitter)
+        layout.addWidget(self.editPanel)
         # layout.addWidget(self.adminListView)
         # layout.addWidget(self.creatorListView)
 
