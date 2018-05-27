fe9155d8d15f20e20024896ff8a42f3361ced894
= <->
2018-05-27 16:46:19 +0700

    [ apply the change to main ]

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QHBoxLayout, QSplitt
 from PyQt5.QtCore import Qt
 from qtmodern import styles, windows
 import sys
+from TConnect import connector
 
 
 class ManageUserWindow(QWidget):
@@ -15,19 +16,17 @@ class ManageUserWindow(QWidget):
         self.adminListView = UserListView()
         self.adminListView.setTitleText("Admin User List")
         self.adminListView.setIcon("img/admin-icon.png")
+        self.adminListView.setOnSelectUser(self.__onClickAdmin)
 
-        self.creatorListView = UserListView()
+        self.creatorListView = UserListView(keys=['id', 'name', 'email'])
         self.creatorListView.setTitleText("Creator User List")
         self.creatorListView.setIcon("img/artist-icon.png")
+        self.creatorListView.setOnSelectUser(self.__onClickCreator)
 
         self.editPanel = EditPanel()
         self.editPanel.setFixedWidth(300)
-        self.editPanel.addEditRow('Employment ID', QLineEdit)
         self.editPanel.addEditRow('Display Name', QLineEdit)
-        self.editPanel.addEditRow('Username', QLineEdit)
-        self.editPanel.addEditRow('Password', QLineEdit)
         self.editPanel.addEditRow('Email', QLineEdit)
-        self.editPanel.addEditRow('Notes', QTextEdit)
 
         self.splitter = QSplitter(Qt.Vertical)
         self.splitter.addWidget(self.adminListView)
@@ -42,19 +41,32 @@ class ManageUserWindow(QWidget):
 
         self.__initialize()
 
+    def __onClickCreator(self, creator):
+        print("__onClickCreator called")
+        self.editPanel.setEditValue('Display Name', creator['name'])
+        self.editPanel.setEditValue('Email', creator['email'])
+        self.editPanel.setAdditionalData(creator)
+
+    def __onClickAdmin(self, admin):
+        print("__onClickAdmin called")
+
     def __initialize(self):
-        pass
+        def onreceive(result):
+            for k, v in result.items():
+                v['id'] = k
+                self.creatorListView.addUser(v)
+                print(k, v)
 
+        def onerror():
+            print('get creator list error occurred')
 
-def verification(u, p):
-    return u == 'admin' and p == 'password'
+        connector.getCreatorList(onreceive, onerror)
 
 
 app = QApplication(sys.argv)
 styles.dark(app)
 
 dialog = LoginDialog()
-dialog.setVerification(verification)
 mdialog = windows.ModernWindow(dialog)
 mdialog.show()
 loginResult = dialog.exec_()
@@ -65,4 +77,5 @@ if loginResult != QDialog.Accepted:
 userwin = ManageUserWindow()
 _userwin = windows.ModernWindow(userwin)
 _userwin.show()
+
 sys.exit(app.exec_())
